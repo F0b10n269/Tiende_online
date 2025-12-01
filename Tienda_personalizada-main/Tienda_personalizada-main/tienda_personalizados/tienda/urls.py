fad5b774292aff675c66c8d0b2151eef5465446a
F0b10n269 <fabian.calderon07@inacapmail.cl>
2025-11-30 22:43:52 -0300

    Update urls.py

--- Tienda_personalizada-main/Tienda_personalizada-main/tienda_personalizados/tienda/urls.py
+++ Tienda_personalizada-main/Tienda_personalizada-main/tienda_personalizados/tienda/urls.py
@@ -4,12 +4,11 @@ from . import views
 app_name = 'tienda'
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('catalogo/', views.CatalogoView.as_view(), name='catalogo'),
-    path('producto/<int:pk>/', views.DetalleProductoView.as_view(), name='detalle_producto'),
-    path('solicitar-pedido/', views.SolicitarPedidoView.as_view(), name='solicitar_pedido'),
+    path('', views.index, name='index'),
+    path('catalogo/', views.catalogo, name='catalogo'),
+    path('producto/<int:pk>/', views.detalle_producto, name='detalle_producto'),
+    path('solicitar-pedido/', views.solicitar_pedido, name='solicitar_pedido'),
     path('pedido-exitoso/', views.pedido_exitoso, name='pedido_exitoso'),
-
-    # URL correcta para seguimiento
+    # CAMBIAR: Usar <str:token> en lugar de <uuid:token>
     path('seguimiento/<str:token>/', views.seguimiento_pedido, name='seguimiento_pedido'),
 ]
