b277154544037f5333d4808eef46d7f460485ba5
BlakeMercer99 <rodrigocofre98@gmail.com>
2025-12-16 22:00:50 +0000

    otra correcion mas con respecto a la carpeta media

--- Tienda_personalizada-main/Tienda_personalizada-main/tienda_personalizados/tienda_personalizados/settings.py
+++ Tienda_personalizada-main/Tienda_personalizada-main/tienda_personalizados/tienda_personalizados/settings.py
@@ -139,4 +139,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/static/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
